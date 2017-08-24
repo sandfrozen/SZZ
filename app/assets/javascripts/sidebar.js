5dcae9f6d7adaaabcae92471db1786e60de201a1
Tomek Buslowski <tbuslowski@gmail.com>
2017-08-24 17:41:09 +0200

    js works in brownie

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
-	// jquery
+	$('#toggle-btn').on('click', function (e) {
+
+        e.preventDefault();
+
+        if ($(window).outerWidth() > 1194) {
+            $('nav.side-navbar').toggleClass('shrink');
+            $('.page').toggleClass('active');
+        } else {
+            $('nav.side-navbar').toggleClass('show-sm');
+            $('.page').toggleClass('active-sm');
+        }
+    });
 });
